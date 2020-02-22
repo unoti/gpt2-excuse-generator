85adbfd22c0251d6f961e52b72926275ec9e50e8
unoti <unoti2@gmail.com>
2020-02-22 12:54:36 -0800

    ui work

--- sample_client/src/components/ExcuseInput.tsx
+++ sample_client/src/components/ExcuseInput.tsx
@@ -12,19 +12,41 @@ type ExcuseInputProps = {
 
 export const ExcuseInput: FunctionComponent<ExcuseInputProps> = ({scenario, onSubmit}) => {
     const assignmentFieldId = 'assignmentField';
-    const stepIds = ['step1','step2','step3','step4','step5']
+    const stepIds = ['step1','step2','step3','step4','step5'];
+    // useState returns 2 values: currentState, and a function that updates it.
+    const [sc, setScenario] = useState(scenario);
+    function assignmentChange(e:any) {
+        let val = e.target.defaultValue;
+        setScenario({...sc, assignment: val});
+    }
+    function taskChange(seq: number, val: string) {
+        let tasks = sc.tasks.slice(); // clone list.
+        tasks[seq] = val;
+        setScenario({...sc, tasks: tasks});
+    }
+    function updateTask(seq: number) {
+        function handler(e: any) {
+            let val = e.target.defaultValue;
+            taskChange(seq, val);
+        }
+        return handler;
+    }
+    function submit() {
+        onSubmit(sc);
+    }
+
     return (
         <div>
             <h1>Excuse</h1>
             <Label htmlFor={assignmentFieldId}>My assignment was to</Label>
-            <TextField id={assignmentFieldId} value={scenario.assignment}/>
+            <TextField id={assignmentFieldId} value={sc.assignment} onChange={assignmentChange}/>
             <div>Steps I would take:</div>
-            <TextField id={stepIds[0]} value={scenario.tasks[0]}/>
-            <TextField id={stepIds[1]} value={scenario.tasks[1]}/>
-            <TextField id={stepIds[2]} value={scenario.tasks[2]}/>
-            <TextField id={stepIds[3]} value={scenario.tasks[3]}/>
-            <TextField id={stepIds[4]} value={scenario.tasks[4]}/>
-            <PrimaryButton text="Make Excuses" onClick={onSubmit(scenario)}/>
+            <TextField id={stepIds[0]} value={sc.tasks[0]} onChange={updateTask(0)}/>
+            <TextField id={stepIds[1]} value={sc.tasks[1]} onChange={updateTask(1)}/>
+            <TextField id={stepIds[2]} value={sc.tasks[2]} onChange={updateTask(2)}/>
+            <TextField id={stepIds[3]} value={sc.tasks[3]} onChange={updateTask(3)}/>
+            <TextField id={stepIds[4]} value={sc.tasks[4]} onChange={updateTask(4)}/>
+            <PrimaryButton text="Make Excuses" onClick={submit}/>
         </div>
     );
 }
