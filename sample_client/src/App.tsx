0c4462384129ea54d748a861d6386dafd1e0c259
unoti <unoti2@gmail.com>
2020-02-22 12:02:54 -0800

    ui work

--- sample_client/src/App.tsx
+++ sample_client/src/App.tsx
@@ -1,36 +1,58 @@
 import React from 'react';
-import { Stack, Text, Link, FontWeights } from 'office-ui-fabric-react';
+import { Stack, Text, FontWeights } from 'office-ui-fabric-react';
 
+import { ExcuseScenario } from './models';
 import ShrugImage from './images/tanya.png';
 import './App.css';
 import { ExcuseInput } from './components/ExcuseInput';
 
 const boldStyle = { root: { fontWeight: FontWeights.semibold } };
 
+const startScenario: ExcuseScenario = {
+  assignment: 'take the trash out and clean the kitchen',
+  tasks: [
+    'assess the current trash levels',
+    'gather up the loose overflow trash items',
+    'remove the bag from the can',
+    'tie the bag',
+    'take it to the outside bin',
+    'put a new bag into the can'
+  ]
+};
+
+function onGenerate(scenario: ExcuseScenario) {
+  console.log('Generate');
+  console.log(scenario);
+}
+
 export const App: React.FunctionComponent = () => {
   return (
-    <Stack
-      horizontalAlign="center"
-      verticalFill
-      styles={{
-        root: {
-          width: '960px',
-          margin: '0 auto',
-          textAlign: 'center',
-          color: '#605e5c'
-        }
-      }}
-      gap={15}
-    >
-      <img src={ShrugImage} alt="shrug" />
-      <Text variant="xxLarge" styles={boldStyle}>
-        Excuse Generator
-      </Text>
-      <div>
-        <a href="https://svgsilh.com/png/1641898.png">Tanya</a> image is licensed under&nbsp;
-        <a href="https://creativecommons.org/licenses/cc0/1.0/?ref=ccsearch&atype=html">CC 1.0</a>
-      </div>
-      <ExcuseInput assignment="test 123"/>
+    <Stack>
+      <Stack
+        horizontalAlign="center"
+        verticalFill
+        styles={{
+          root: {
+            width: '960px',
+            margin: '0 auto',
+            textAlign: 'center',
+            color: '#605e5c'
+          }
+        }}
+        gap={15}
+      >
+        <img src={ShrugImage} alt="shrug" />
+        <Text variant="xxLarge" styles={boldStyle}>
+          Excuse Generator
+        </Text>
+        <div>
+          <a href="https://svgsilh.com/png/1641898.png">Tanya</a> image is licensed under&nbsp;
+          <a href="https://creativecommons.org/licenses/cc0/1.0/?ref=ccsearch&atype=html">CC 1.0</a>
+        </div>
+      </Stack>
+      <Stack>
+        <ExcuseInput scenario={startScenario} onSubmit={onGenerate}/>
+      </Stack>
     </Stack>
   );
 };
--- sample_client/src/components/ExcuseInput.tsx
+++ sample_client/src/components/ExcuseInput.tsx
@@ -1,19 +1,30 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { Label } from 'office-ui-fabric-react/lib/Label';
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
+import { PrimaryButton } from 'office-ui-fabric-react';
+
+import { ExcuseScenario } from '../models';
 
 type ExcuseInputProps = {
-    assignment: string,
+    scenario: ExcuseScenario,
+    onSubmit: any
 }
 
-export const ExcuseInput: FunctionComponent<ExcuseInputProps> = ({assignment}) => {
+export const ExcuseInput: FunctionComponent<ExcuseInputProps> = ({scenario, onSubmit}) => {
     const assignmentFieldId = 'assignmentField';
+    const stepIds = ['step1','step2','step3','step4','step5']
     return (
-        <aside>
+        <div>
             <h1>Excuse</h1>
             <Label htmlFor={assignmentFieldId}>My assignment was to</Label>
-            <TextField id={assignmentFieldId} />
-        </aside>
+            <TextField id={assignmentFieldId} value={scenario.assignment}/>
+            <div>Steps I would take:</div>
+            <TextField id={stepIds[0]} value={scenario.tasks[0]}/>
+            <TextField id={stepIds[1]} value={scenario.tasks[1]}/>
+            <TextField id={stepIds[2]} value={scenario.tasks[2]}/>
+            <TextField id={stepIds[3]} value={scenario.tasks[3]}/>
+            <TextField id={stepIds[4]} value={scenario.tasks[4]}/>
+            <PrimaryButton text="Make Excuses" onClick={onSubmit(scenario)}/>
+        </div>
     );
 }
- 
