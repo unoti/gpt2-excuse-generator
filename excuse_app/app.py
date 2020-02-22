229342c76dcd50e39e4f77cc9e5c88764008a0cb
unoti <unoti2@gmail.com>
2020-02-22 15:48:26 -0800

    websockets start

--- excuse_app/app.py
+++ excuse_app/app.py
@@ -1,21 +1,26 @@
 print('App Loading')
 import os
 from flask import Flask, request
+from flask_socketio import SocketIO
 
 from excuses import ExcuseSituation
 from textgen import TextGenerator
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'so secret'
+socketio = SocketIO(app, cors_allowed_origins=['http://localhost:3000'])
 
 #model_size = 'gpt2-xl'  # Too much for CPU-based inference.
-model_size = 'gpt2'
-gen = TextGenerator(model_size)
+
+#model_size = 'gpt2'
+#gen = TextGenerator(model_size)
 
 @app.route('/excuse')
 def hello_world():
     resp = {'What': 'Excuse Generator', 'output': 'something13'}
     return resp
 
+"""
 @app.route('/excuse/gen')
 def excuse():
     def show_word(w):
@@ -32,10 +37,15 @@ def excuse():
         'excuses' : excuses
     }
     return result
+"""
+
+@socketio.on('my event')
+def handle_message(data):
+    print('< ' + str(data))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
     print('App Ready')
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
 
 
--- sample_client/src/services/ExcuseGenerator.ts
+++ sample_client/src/services/ExcuseGenerator.ts
@@ -1,18 +1,28 @@
+import * as socketIo from 'socket.io-client';
+
 import { ExcuseScenario } from '../models';
+
+const SERVER_URL = 'http://localhost:5000'
 
 export class ExcuseGeneratorSession {
     scenario: ExcuseScenario;
     excuses: string[];
     updateCallback: any;
+    socket: any;
 
     constructor(scenario: ExcuseScenario, onUpdated: any) {
         this.scenario = scenario;
         this.excuses = [];
         this.updateCallback = onUpdated;
+        this.socket = null;
     }
 
     startGenerating = () => {
         console.log('start');
+        this.socket = socketIo.connect(SERVER_URL);
+        this.socket.on('connect', () => {
+            this.socket.emit('my event', {'data': 'I have connected'});
+        });
         setTimeout(this.updateCallback, 1000);
     }
 
